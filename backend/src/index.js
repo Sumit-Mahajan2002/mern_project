66353d714f9c7d5b4cb4bcf2631c7d04b4f74328
Sumit-Mahajan2002 <sumitsmahajan1111@gmail.com>
2025-02-27 18:54:48 +0530

    Added new Functionalities in Frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-
+import cors from "cors";
 import {connectDB} from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -15,6 +15,12 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes)
 app.use("/api/auth", messageRoutes)
